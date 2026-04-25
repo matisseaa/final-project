4edc312f26e697a44760b61e0d8436cebdd53ee4
tshopova <tshopova.ieu2025@student.ie.edu>
2026-04-25 18:00:54 +0200

    Add dependency requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "numpy>=1.26",
+        "matplotlib>=3.8",
+        "pyfiglet>=1.0"
+    ],
 )
-
